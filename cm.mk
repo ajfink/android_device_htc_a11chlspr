a053b50dc1ee1e24070dbfacc1d3ad6a280758ee
ajfink <andrew.finkenbinder@gmail.com>
2014-12-10 07:51:57 -0500

    mkvendor.sh: Initial commit of a11chlspr
    Change-Id: Idca2de323aa8f8b0e242be0e6f7cfef5670d1a8f

---
+++ cm.mk
@@ -0,0 +1,18 @@
+## Specify phone tech before including full_phone
+$(call inherit-product, vendor/cm/config/gsm.mk)
+
+# Release name
+PRODUCT_RELEASE_NAME := a11chlspr
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/htc/a11chlspr/device_a11chlspr.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := a11chlspr
+PRODUCT_NAME := cm_a11chlspr
+PRODUCT_BRAND := htc
+PRODUCT_MODEL := a11chlspr
+PRODUCT_MANUFACTURER := htc
